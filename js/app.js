9b857591f6671eabd92f94c22ee0c809ad77b860
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-09-24 23:34:15 -1000

    help pls

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 /*Exercise 1.  Fill in the correct id to target the paragraph tag*/
   function fontSize(){
-    document.getElementById(______).style.fontSize = "200%";
+    document.getElementById('biggie').style.fontSize = "200%";
   }
 
  /*Exercise 2. change the font color to red*/
   function colorMeBadd(){
-    document.getElementById(_______).style.color = '______'; 
+    document.getElementById('colors').style.color = 'red'; 
   }
 
 /*Exercise 3. fill in the spaces to make this function operate:*/
   function showAnswer(){
-    document.getElementById(_______).________ = " C (da sea get it?) arrrr!";
+    document.getElementById('answer').innerHTML = " C (da sea get it?) arrrr!";
   }
 
 /*Exercise 4.  Instruct the following function to display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
 
   function kungfu(){
-    ________.getElementById(________).________ = "My name is Lee, Brrruuuce Lee!";
+    document.getElementById('beWater').innerHTML = "My name is Lee, Brrruuuce Lee!";
   }
 
 
@@ -26,28 +26,55 @@
 window.onload = function (){
 /*Exercise 5. Add an event listener to the quotes button that will display a quote after it is clicked on.</p>*/
 
-var firstQuote = "Our lives are defined by opportunities; even the ones we miss.";
+var firstQuote = "When you do things right, people won't be sure you've done anything at all";
 
-_________.getElementById(________).addEventListener(______, sayQuote);
+document.getElementById('Benjamin').addEventListener('click', sayQuote);
 
 function sayQuote(){
-  document._________(_______).__________ = __________;
+  document.getElementById('lines').innerHTML = firstQuote;
 }
 
 
 /*Exercise 6. Add another event listener to the image below that will display an inspirational quote after it is clicked</p>*/
 
+var myQuote = "knowing is better than learning..";
 
+document.getElementById('khaled').addEventListener('click', khaledQuote);
+
+function khaledQuote(){
+  document.getElementById('wedabest').innerHTML = myQuote;
+}
 
 
 /*Exercise 7.  Generate a random quote from clicking on a button*/
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
+
+document.getElementById('change').addEventListener('click', randomQuote);
+
+function randomQuote(){
+  document.getElementById('displayQuotes').innerHTML = quotes[RNG()];
+}
+
+function RNG(){
+  var randomNumber = Math.floor(Math.random()*quotes.length);
+  return randomNumber;
+}
 
 
                                                    
 /*Exercise 8. Create an event listener that will show and hide the message when clicking on the button.*/
 
 
+
+document.getElementById("hideandseek").addEventListener('click', showMessage);
+
+function showMessage(){
+  var theMessage = document.getElementById("showmoney");
+  if(theMessage.style.display === 'none'){theMessage.style.display = 'block';}
+    else{
+      theMessage.style.display = 'none';
+    }
+}
 
 /*Final Boss:
 Make a visual list of the items in the orders array. When clicking on the product, you must show and hide the price for that item.*/
@@ -56,8 +83,38 @@ Make a visual list of the items in the orders array. When clicking on the produc
 var orders = [{product: "Finger Toothbrush", price: 9.99}, {product: "Barry Manilow's Greatest Hits Collection Vol 1", price: 500.01}, {product: "Goldfish Aquairum Phone Booth", price: 199.99}, {product: "Woof Washer 360", price: 9.99}, {product: "Sauna Pants", price: 39.95}, {product: "Noiseless Mute Karaoke Microphone", price: 79.99}, {product: "Necktie Umbrella", price: 16.86}];
 
 
-};
 
+var myOrders = document.getElementById('myOrders');
+
+function createButtons(){
+  for(var i=0; i<orders.length; i++){
+    var newButton = document.createElement('button');
+    var newSpan1 = document.createElement('span');
+    newSpan1.innerHTML = orders[i].product;
+    newButton.appendChild(newSpan1);
+    newSpan1.id = "newSpan1";
+    var newSpan2 = document.createElement('span');
+    newSpan2.innerHTML = orders[i].price;
+    newButton.appendChild(newSpan2);
+    newSpan2.id = "newSpan2";
+    newSpan2.style.display = 'none';
+    newButton.addEventListener('click', togglePrice);
+    myOrders.appendChild(newButton);
+  }
+}
+
+createButtons();
+
+function togglePrice(){
+  var thePrice = document.getElementById("newSpan2");
+  if (thePrice.style.display === 'none'){thePrice.style.display = 'block';
+} else {
+  thePrice.style.display = 'none';
+}
+}
+
+
+};
 
 
 
